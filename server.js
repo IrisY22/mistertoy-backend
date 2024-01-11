d9aade886099d737e49911174ca5cf088d64caa5
Iris Yatskaer <iriska1160@gmail.com>
2024-01-11 16:24:20 +0200

    fix url backend

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const corsOptions = {
     "http://localhost:8080",
     "http://127.0.0.1:5175",
     "http://localhost:5175",
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://127.0.0.1:5174",
+    "http://localhost:5174",
   ],
   credentials: true,
 };
